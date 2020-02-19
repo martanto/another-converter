765633cbc8eaefcd50c9b58bf8691e8395ecffa5
Martanto <martanto@live.com>
2020-02-19 09:35:36 +0700

    Update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime
 import os
 from obspy import Stream, Trace, read
 import numpy as np
+import pandas as pd
 import orator
 from models.sds_index import SdsIndex
 import multiprocessing
@@ -27,7 +28,7 @@ class Configuration():
     def get(self):
         with open(self.get_location()) as file_config:
             load_config = json.load(file_config)
-            type = load_config['default']
+            get_config = load_config['default']
             start_date = datetime.datetime.strptime(load_config['start_date'],'%Y-%m-%d')
             end_date = datetime.datetime.strptime(load_config['end_date'],'%Y-%m-%d')
             config = {
@@ -35,8 +36,10 @@ class Configuration():
                 'start_date' : start_date,
                 'end_date' : end_date,
                 'output_directory' : load_config['output_directory'],
-                'dayplot_directory' : load_config['dayplot_directory'],
-                'channels' : load_config['type'][type]['channels'] if type == 'sac' else []
+                'index_directory' : os.path.join(load_config['output_directory'], 'Index'),
+                'converted_directory' : os.path.join(load_config['output_directory'], 'Converted'),
+                'dayplot_directory' : os.path.join(load_config['output_directory'], 'Dayplots'),
+                'channels' : load_config['type'][get_config]['channels'] if get_config == 'sac' else []
             }
             return config
 
@@ -51,14 +54,16 @@ class Files():
         if len(stream_files) > 0:
             new_stream = Stream()
             for stream in stream_files:
-                read_stream = read(stream)
-                for trace in read_stream:
-                    if trace.stats.sampling_rate < 50.0:
-                        read_stream.remove(trace)
-                new_stream+=read_stream
+                try:
+                    read_stream = read(stream)
+                    for trace in read_stream:
+                        if trace.stats.sampling_rate < 50.0:
+                            read_stream.remove(trace)
+                    new_stream+=read_stream
+                except:
+                    print('Error : '+stream)
             new_stream.merge(fill_value=0)
             return new_stream
-        pass
 
     def search_sac(self, date):
         search_list = []
@@ -129,13 +134,15 @@ class NewTrace():
         return trace
 
 class Convert():
-    def __init__(self, location='config.json', save_to_database=False):
+    def __init__(self, location='config.json', save_to_database=False, save_to_csv=False):
         self.files = Files()
         self.sds = SDS()
         self.save_index = save_to_database
+        self.save_csv = save_to_csv
         self.index = SaveIndex()
         self.config = Configuration(location).get()
-        self.output = self.config['output_directory']
+        self.index_directory = self.config['index_directory']
+        self.output = self.config['converted_directory']
 
     def date_range(self):
         start_date = self.config['start_date']
@@ -159,10 +166,13 @@ class Convert():
             if new_trace.stats.sampling_rate >= 50.0:
                 print(new_trace)
                 path = self.sds.save(self.output,new_trace)
-                self.index.save(path, new_trace, date)
+                if self.save_index:
+                    self.index.save(path, new_trace, date, db=True)
+                if self.save_csv==True:
+                    self.index.save(path, new_trace, date, csv=True, index_directory=self.index_directory)
             else:
-                print('Skipped : %s' %new_trace)
-        print('DONE!!')
+                print('Skipped '+date.strftime('%Y-%m-%d'))
+        print(':: '+date.strftime('%Y-%m-%d')+' DONE!!')
 
 class SDS():
     def __init__(self):
@@ -231,11 +241,11 @@ class SaveIndex():
         return availability
 
     def get_filesize(self,filename):
-        file_mseed = os.path.join(Configuration().get()['output_directory'], filename)
+        file_mseed = os.path.join(Configuration().get()['converted_directory'], filename)
         trace = read(file_mseed)[0]
         return trace.stats.mseed.filesize
 
-    def save(self, filename, trace, date):
+    def save(self, filename, trace, date, db=False, csv=False, index_directory=None):
         attributes = {
             'scnl':self.get_scnl(trace),
             'date':date,
@@ -248,12 +258,37 @@ class SaveIndex():
             'availability':self.get_availability(trace),
             'filesize':self.get_filesize(filename)
         }
-        
-        SdsIndex.update_or_create(attributes=attributes, values=values)
 
+        if db:
+            SdsIndex.update_or_create(attributes=attributes, values=values)
+
+        if csv:
+            if not os.path.exists(index_directory):
+                os.makedirs(index_directory)
+
+            df = {
+                'scnl' : [attributes['scnl']],
+                'date' : [attributes['date']],
+                'sampling_rate' : [values['sampling_rate']],
+                'max_amplitude' : [values['max_amplitude']],
+                'availability' : [values['availability']],
+                'filesize' : [values['filesize']],
+            }
+            
+            df = pd.DataFrame(df)
+
+            file_csv = os.path.join(index_directory,attributes['scnl']+'.csv')
+
+            if not os.path.isfile(file_csv):
+                df.to_csv(file_csv, header=['scnl','date','sampling_rate','max_amplitude','availability','filesize'], index=False, date_format='%Y-%m-%d')
+            else:
+                df.to_csv(file_csv, mode='a', header=False, index=False, date_format='%Y-%m-%d')
+            
 def main():
     print("Jumlah CPU : ", multiprocessing.cpu_count())
-    Convert(save_to_database=True).to_mseed(10)
+    cpu = int(multiprocessing.cpu_count()/2)
+    print('CPU yang digunakan: '+str(cpu))
+    Convert(save_to_csv=True).to_mseed(cpu)
 
 if __name__ == '__main__':
     main()
