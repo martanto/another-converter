d4c5ac16b4e51970ed098f4258da5201544be00b
Martanto <martanto@live.com>
2023-07-18 21:29:31 +0700

    update database migration

--- databases/migrations/2020_01_13_063357_create_seismic_data_table.py
+++ databases/migrations/2020_01_13_063357_create_seismic_data_table.py
@@ -1,4 +1,4 @@
-from orator.migrations import Migration
+from masoniteorm.migrations import Migration
 
 class CreateSeismicDataTable(Migration):
 
@@ -6,20 +6,19 @@ class CreateSeismicDataTable(Migration):
         """
         Run the migrations.
         """
-        with self.schema.create('sds_indexes') as table:
+        with self.schema.create('seismic_data') as table:
             table.big_increments('id')
             table.string('filename')
             table.string('scnl')
             table.date('date')
             table.float('sampling_rate').default(0.0)
-            table.float('min_amplitude').default(0.0)
             table.float('max_amplitude').default(0.0)
             table.float('availability').default(0.0)
             table.big_integer('filesize').default(0)
             table.unique(['scnl','date'])
             table.index('date')
             table.index('scnl')
-            table.nullable_timestamps()
+            table.timestamps().nullable()
 
     def down(self):
         self.schema.drop_if_exists('seismic_data')
